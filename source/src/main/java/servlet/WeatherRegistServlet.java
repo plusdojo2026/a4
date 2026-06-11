b8f9d948fcf1b29405844223dccd767aedd81413
plusdojo2026-yokoyama <yokoyama-toshiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-11 11:40:02 +0900

    天気サーブレットの編集

--- source/src/main/java/servlet/WeatherRegistServlet.java
+++ source/src/main/java/servlet/WeatherRegistServlet.java
@@ -1,41 +1,54 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class WeatherRegistServlet
- */
+
 @WebServlet("/WeatherRegistServlet")
 public class WeatherRegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public WeatherRegistServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+    
+	//Getの方
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("loginUser") == null) {
+			response.sendRedirect("LoginServlet");
+			return;
 	}
+		// 天気登録ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/WeatherRegist.jsp");
+				dispatcher.forward(request, response);
+			}
+	
+	
+	//Postの方
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("loginUser") == null) {
+			response.sendRedirect("LoginServlet");
+			return;
+		}
+		
+		
+		// 数値から変換してフォーム埋めたい(これはJSP側か？)
+		// リクエストパラメータを取得する
+		
+		//　登録処理する
+			
+		// ホームのページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/home.jsp");
+		dispatcher.forward(request, response);
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+}
 }

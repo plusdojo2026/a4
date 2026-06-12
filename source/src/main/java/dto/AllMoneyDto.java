ecbb5550c8fb3e4b847ce42184e77503b7fdd7f2
plusdojo2026-yokoyama <yokoyama-toshiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-12 10:41:56 +0900

    AllMoneyの直し

--- source/src/main/java/dto/AllMoneyDto.java
+++ source/src/main/java/dto/AllMoneyDto.java
@@ -4,62 +4,56 @@ import java.time.LocalDate;
 
 // フィールド
 public class AllMoneyDto {
-	private int feed_id;
+	private int money_id;
+	private int income;
+	private int expense;
 	private LocalDate date;
-	private int increase_amount;
-	private int decrease_amount;
 	
 	// 空コンストラクタ
 	public AllMoneyDto() {
+		
 	}
 	
 	// コンストラクタ
-	public AllMoneyDto(int feed_id, LocalDate date, int increase_amount, int decrease_amount) {
+	public AllMoneyDto(int money_id, int income, int expense, LocalDate date) {
 		super();
-		this.feed_id = feed_id;
+		this.money_id = money_id;
+		this.income = income;
+		this.expense = expense;
 		this.date = date;
-		this.increase_amount = increase_amount;
-		this.decrease_amount = decrease_amount;
+	}
+	
+	// getterとsetter
+	public int getMoney_id() {
+		return money_id;
 	}
 
-	// 以下、getterとsetter
-	public int getFeed_id() {
-		return feed_id;
+	public void setMoney_id(int money_id) {
+		this.money_id = money_id;
 	}
 
-
-	public void setFeed_id(int feed_id) {
-		this.feed_id = feed_id;
+	public int getIncome() {
+		return income;
 	}
 
+	public void setIncome(int income) {
+		this.income = income;
+	}
+
+	public int getExpense() {
+		return expense;
+	}
+
+	public void setExpense(int expense) {
+		this.expense = expense;
+	}
 
 	public LocalDate getDate() {
 		return date;
 	}
 
-
 	public void setDate(LocalDate date) {
 		this.date = date;
-	}
-
-
-	public int getIncrease_amount() {
-		return increase_amount;
-	}
-
-
-	public void setIncrease_amount(int increase_amount) {
-		this.increase_amount = increase_amount;
-	}
-
-
-	public int getDecrease_amount() {
-		return decrease_amount;
-	}
-
-
-	public void setDecrease_amount(int decrease_amount) {
-		this.decrease_amount = decrease_amount;
 	}
 	
 	

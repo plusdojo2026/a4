e8f0befacee595113eee8802a9b0f6a656f24b79
plusdojo2026-yoshida <yoshida-kenji-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-12 11:21:57 +0900

    エサdtoの編集

--- source/src/main/java/dto/FeedsDto.java
+++ source/src/main/java/dto/FeedsDto.java
@@ -1,5 +1,69 @@
 package dto;
 
-public class FeedsDto {
+import java.io.Serializable;
+import java.time.LocalDate;
 
+/*
+ * Serializableについて
+ * インスタンスをバイト列に変換（シリアライズ）可能にする
+ * 主に、ネットワーク送信やセッション保存を可能にするために必要
+ */
+
+// フィールド
+// ログイン処理の際は下3つを持ってく処理にする
+
+public class FeedsDto implements Serializable{
+	private int money_id;
+	private int income;
+	private int expense;
+	private LocalDate date;
+	
+	//コンストラクタ
+	public FeedsDto(int money_id, int income, int expense, LocalDate date) {
+		super();
+		this.money_id = money_id;
+		this.income = income;
+		this.expense = expense;
+		this.date = date;
+	} 
+	
+	//空のコンストラクタ
+	public FeedsDto() {
+	}
+	
+	
+	//以下、getterとsetter
+	public int getMoney_id() {
+		return money_id;
+	}
+
+	public void setMoney_id(int money_id) {
+		this.money_id = money_id;
+	}
+
+	public int getIncome() {
+		return income;
+	}
+
+	public void setIncome(int income) {
+		this.income = income;
+	}
+
+	public int getExpense() {
+		return expense;
+	}
+
+	public void setExpense(int expense) {
+		this.expense = expense;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	
 }

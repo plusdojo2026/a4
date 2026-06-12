760d53e5dadb0826eed66f4939831b597a71257b
plusdojo2026-yoshida <yoshida-kenji-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-12 16:32:13 +0900

    Daoとサーブレット途中まで編集

--- source/src/main/java/dao/FeedsDao.java
+++ source/src/main/java/dao/FeedsDao.java
@@ -1,5 +1,80 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class FeedsDao {
 
-}
+	// 引数card指定された項目で検索して、取得されたデータのリストを返す
+	public List<FeedsDto> select(FeedsDto feeds) {
+		List<FeedsDto> feedsAmount = new ArrayList<FeedsDto>();
+		
+		//接続状態
+		Connection conn = null;
+		
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a4?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"root", "password");
+
+			// SQL文を準備する
+			String sql = "SELECT * FROM feeds WHERE increase_amount =? AND decrease_amount=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, feeds.getIncrease_amount());
+			pStmt.setString(2, feeds.getDecrease_amount());
+			
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				FeedsDto feeds = new FeedsDto(
+						rs.getInt("feed_id"), 
+						rs.getDate("date"),
+						rs.getInt("increase_amount"),
+						rs.getInt("decrease_amount")
+						);
+				feedsAmount.add(feeds);
+			}
+			
+			//以下、例外処理
+		} catch (SQLException e) {
+			e.printStackTrace();
+			feedsAmount = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			feedsAmount = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					feedsAmount = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return feedsAmount;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+｝
--- source/src/main/webapp/js/MoneyMonthly.js
+++ source/src/main/webapp/js/MoneyMonthly.js
@@ -1,3 +1,42 @@
 /**
- * 
+ * 円グラフのjs
  */
+ 
+ <script>
+//顔文字のドーナツチャートの部分の処理
+let inputData =${kaoList};
+let context = document.querySelector("#kimochi").getContext('2d')
+new Chart(context, {
+  type: 'doughnut',
+  data: {
+    labels: ["にっこり", "ちょっとにっこり", "普通", "ちょっと不満", "不満"],
+    datasets: [{
+      //ここで取得した配列の中身を分解して配置する
+      data: [inputData[0], inputData[1],inputData[2],inputData[3],inputData[4],],
+      backgroundColor: ['#7fffd4', '#87cefa', '#f5deb3','#ff69b4','#dc143c'],
+    }]
+  },
+  options: {
+    responsive: false,
+  }
+});
+
+let inputData2 =${xxList};
+//二つ以上グラフを書くときはcontext2,3,4...となんでもいいので名前を変更する
+let context2 = document.querySelector("#kimo").getContext('2d')
+//上記を変更した場合は、この下の名前（context）も上の名前に合わせる
+new Chart(context2, {
+  type: 'pie',
+  data: {
+    labels: ["うひ", "うほ", "うき", "きき", "んご"],
+    datasets: [{
+      //ここで取得した配列の中身を分解して配置する
+      data: [inputData2[0], inputData2[1],inputData2[2],inputData2[3],inputData2[4],],
+      backgroundColor: ['#87cefa', '#7cfc00', '#d2691e','#dda0dd','#ffebcd'],
+    }]
+  },
+  options: {
+    responsive: false,
+  }
+});
+</script>

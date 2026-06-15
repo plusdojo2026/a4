1e3b0a210c9a610fb8818e82c5f12d97d5254ea9
plusdojo2026-yamaguchi <yamaguchi-yu-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 13:08:18 +0900

    Shift DAOとDTOの編集
    シフトｎDTOにnameはいらなそうだから抜いた
    DAOもそれにあわせていろいろ整えた

--- source/src/main/java/dto/ShiftDto.java
+++ source/src/main/java/dto/ShiftDto.java
@@ -4,14 +4,12 @@ import java.io.Serializable;
 
 public class ShiftDto implements Serializable{
 	private int id; //従業員ID
-	private String name; //名前
 	private int intime; //入る時間
 	private String date; //日付
 	
-	public ShiftDto(int id, String name, int intime, String date) {
+	public ShiftDto(int id, int intime, String date) {
 		super();
 		this.id = id;
-		this.name = name;
 		this.intime = intime;
 		this.date = date;
 	}
@@ -26,14 +24,6 @@ public class ShiftDto implements Serializable{
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public int getIntime() {

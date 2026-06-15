c686d7aea484c6e1a8175f744628339c8fc2654d
plusdojo2026-yokoyama <yokoyama-toshiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-15 11:20:53 +0900

    servlet編集

--- source/src/main/java/servlet/EmployeesRegistServlet.java
+++ source/src/main/java/servlet/EmployeesRegistServlet.java
@@ -10,9 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class EmployeesRegistServlet
- */
 @WebServlet("/EmployeesRegistServlet")
 public class EmployeesRegistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;

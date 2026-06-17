1ad283d2f3a8e9ac4384d5f9205447a4cd228677
plusdojo2026-yokoyama <yokoyama-toshiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 10:59:33 +0900

    従業員一覧の変更

--- source/src/main/java/servlet/EmployeesListServlet.java
+++ source/src/main/java/servlet/EmployeesListServlet.java
@@ -25,7 +25,6 @@ public class EmployeesListServlet extends HttpServlet {
     }
     // GETの文
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
 		HttpSession session =request.getSession();
 		
 		//ログインしていないや、管理者フラグが０出なければ拒否される
@@ -37,8 +36,7 @@ public class EmployeesListServlet extends HttpServlet {
 		EmployeesDao empDao = new EmployeesDao();
 		List<EmployeesDto> empList = empDao.select2(new EmployeesDto());		
 		// empList内のgenderに入ってる数字(1,2,3)をを文字(男、女、他)に置き換えたい
-		
-		// 案　Stringのリストを作る？　for文回しながらif？  
+		// 教えてもらって完成
 		for (EmployeesDto e: empList) {
 			if (e.getGender() ==1) {
 				e.setStrGender("男性");
@@ -49,19 +47,7 @@ public class EmployeesListServlet extends HttpServlet {
 			}
 		}
 		System.out.println(empList.get(0).getStrGender());
-//		//　これもダメだった
-//		String gender = null;
-//		if (empList.gender.equals(1)) gender ="男性";
-//		else if (empList.gender.equals(2)) gender ="女性";
-//		else if (empList.gender.equals(3)) gender ="その他";
 
-		//文字→数字の例　(参照:CowDailyServlet)
-//        String appetiteStr = request.getParameter("appetite");
-//       //〇△✕を数値に変換	
-//        int appetite = 0;
-//        if (appetiteStr.equals("〇")) appetite = 1;
-//        else if (appetiteStr.equals("△")) appetite = 2;
-//        else if (appetiteStr.equals("✕")) appetite = 3;
 	//リクエストスコープに格納
 	request.setAttribute("empList",empList);
 	
@@ -71,7 +57,7 @@ public class EmployeesListServlet extends HttpServlet {
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		
 		doGet(request, response);
 	}
 

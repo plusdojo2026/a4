fec5ab048565c9acf0032e9ffe88d8fec416f22e
plusdojo2026-yamaguchi <yamaguchi-yu-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-26 10:52:49 +0900

    ウシ更新削除で登録と同様に重複チェックと１０桁表示

--- source/src/main/webapp/js/CowsUpdateDelete.js
+++ source/src/main/webapp/js/CowsUpdateDelete.js
@@ -1,3 +1,33 @@
-/**
- * 
- */
+document.addEventListener('DOMContentLoaded', () => {
+
+    const form = document.querySelector("form");
+    const cowIdInput = document.getElementById("cow_id");
+    const errorMessage = document.getElementById("error_message");
+
+    cowIdInput.addEventListener("input", function () {
+
+        this.value = this.value.replace(/[^0-9]/g, "");
+
+        errorMessage.textContent = "";
+    });
+
+    form.addEventListener("submit", function (e) {
+
+        const id = cowIdInput.value;
+
+        if (!/^[0-9]{10}$/.test(id)) {
+
+            e.preventDefault();
+
+            if (id.length == 0) {
+                errorMessage.textContent = "ウシIDを入力してください";
+            } else {
+                errorMessage.textContent = "ウシIDは半角数字10桁で入力してください";
+            }
+
+            cowIdInput.focus();
+        }
+
+    });
+
+});
